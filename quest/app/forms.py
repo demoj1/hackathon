9551ceace3a55bec534b0ef16b2021488850606b
diman3241 <dmitryd.prog@gmail.com>
2016-04-12 10:11:58 +0700

    fix errors message

--- quest/app/forms.py
+++ quest/app/forms.py
@@ -2,11 +2,6 @@ from registration.forms import RegistrationForm
 from app.models import UserProfile
 from django import forms
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
-from crispy_forms.bootstrap import (
-    PrependedText, PrependedAppendedText, FormActions)
-
 
 class CustomUserForm(RegistrationForm):
     username = forms.CharField(max_length=30, required=True, label="Имя пользователя")
@@ -20,20 +15,3 @@ class CustomUserForm(RegistrationForm):
     class Meta:
         model = UserProfile
         fields = ('username', 'group', 'first_name', 'last_name',)
-
-    helper = FormHelper()
-    helper.form_method = 'POST'
-    helper.form_class = 'form-horizontal'
-    helper.label_class = 'col-sm-2'
-    helper.field_class = 'col-sm-5'
-
-    helper.layout = Layout(
-        Field('username', css_class='input-sm'),
-        Field('first_name', css_class='input-sm'),
-        Field('last_name', css_class='input-sm'),
-        Field('email', css_class='input-sm'),
-        Field('group', css_class='input-sm'),
-        Field('password1', css_class='input-sm'),
-        Field('password2', css_class='input-sm'),
-        FormActions(Submit("Регистрация", "Регистрация", css_class='btn-primary'))
-    )

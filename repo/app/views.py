de175d07fcbd0344c7a0d2c35ec8a10201751cf2
diman3241 <dmitryd.prog@gmail.com>
2016-04-29 10:38:25 +0700

    Add view

--- repo/app/views.py
+++ repo/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 
+from forms import CustomUserForm
 from registration.backends.simple.views import RegistrationView
 
 # Create your views here.
@@ -16,3 +17,11 @@ def quest(r):
 def page_404(r):
     return render(r, "404.html")
 
+@login_required
+def profile(r):
+    instance = get_object_or_404(CustomUserForm, r.user.id)
+    form = CustomUserForm(r.POST or None, instance=instance)
+    if form.is_valid():
+        form.save()
+        return redirect("quest")
+    return render(r, "root.html")

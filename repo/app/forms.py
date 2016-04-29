116433ec94fd0ec5fed5b5b80f07664e2b478ff8
diman3241 <dmitryd.prog@gmail.com>
2016-04-29 14:36:34 +0700

    Fix text. Add telephone field

--- repo/app/forms.py
+++ repo/app/forms.py
@@ -9,15 +9,16 @@ class CustomUserForm(RegistrationForm):
     last_name = forms.CharField(max_length=30, required=True, label="Фамилия")
     email = forms.EmailField(required=True, label="E-mail")
     group = forms.CharField(required=True, label="Группа")
+    telephone = forms.CharField(required=True, max_length=12, label="Телефон")
     password1 = forms.PasswordInput()
     password2 = forms.PasswordInput()
 
     class Meta:
         model = UserProfile
-        fields = ('username', 'group', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'group', 'first_name', 'last_name', 'email', 'telephone',)
 
 class ProfileUserForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ("first_name", "last_name", "email", "group",)
+        fields = ("first_name", "last_name", "email", "group", 'telephone',)
 

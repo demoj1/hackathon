020d5df7817102f667e2290739d65cbbb7b5ad78
diman3241 <dmitryd.prog@gmail.com>
2016-04-29 14:50:53 +0700

    Null fix

--- repo/app/models.py
+++ repo/app/models.py
@@ -5,4 +5,4 @@ from django.db import models
 # Create your models here.
 class UserProfile(AbstractUser):
     group = models.CharField(max_length=20, verbose_name="Группа")
-    telephone = models.CharField(max_length=12, verbose_name="Телефон", default="")
+    telephone = models.CharField(max_length=12, verbose_name="Телефон", null=True, default="")

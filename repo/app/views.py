f41d78b126931a2a59a4e6b5c5e95ecfde1bd296
diman3241 <dmitryd.prog@gmail.com>
2016-04-29 10:47:49 +0700

    fix templates

--- repo/app/views.py
+++ repo/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 
@@ -23,5 +24,5 @@ def profile(r):
     form = CustomUserForm(r.POST or None, instance=instance)
     if form.is_valid():
         form.save()
-        return redirect("quest")
+        return quest(r)
     return render(r, "root.html")

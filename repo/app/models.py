dd609a40bce76046e8bc0f14d2d036829693f740
diman3241 <dmitryd.prog@gmail.com>
2016-05-06 11:33:54 +0700

    Add report model

--- repo/app/models.py
+++ repo/app/models.py
@@ -6,3 +6,11 @@ from django.db import models
 class UserProfile(AbstractUser):
     group = models.CharField(max_length=20, verbose_name="Группа")
     telephone = models.CharField(max_length=12, verbose_name="Телефон", null=True, default="")
+
+class Report(models.Model):
+    user = models.ForeignKey(UserProfile)
+
+    heroku_url = models.CharField(max_length=200, default="", null=False)
+    github_url = models.CharField(max_length=200, default="", null=False)
+
+    notes = models.TextField()

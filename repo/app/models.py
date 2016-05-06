ff4109d64afcb0d9b843d7800ac984fa4849aef2
Dmitry Shlagoff <wessmoke@mail.ru>
2016-05-06 12:41:25 +0700

    Migrations fuckery.

--- repo/app/models.py
+++ repo/app/models.py
@@ -7,6 +7,7 @@ class UserProfile(AbstractUser):
     group = models.CharField(max_length=20, verbose_name="Группа")
     telephone = models.CharField(max_length=12, verbose_name="Телефон", null=True, default="")
 
+
 class Report(models.Model):
     user = models.ForeignKey(UserProfile)
 
